b99e8473c7b56e9d2e768fde0bbff255fba1bf41
Claudio Bartoli <claudio.bartoli@vipps.no>
2024-12-02 16:45:44 +0100

    d1 step 1

--- d1/d1.go
+++ d1/d1.go
@@ -1,8 +1,12 @@
 package d1
 
 import (
+	"bufio"
 	"math"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 func ComputeDistance(s1, s2 []int) int {
@@ -42,4 +46,61 @@ func d(s1 []int, s2 []int) int {
 
 func IntAbs(i int) int {
 	return int(math.Abs(float64(i)))
+}
+
+func ParseInput(file *os.File) [][]int {
+	r := bufio.NewReader(file)
+	res := make([][]int, 0)
+	for {
+		line, _, err := r.ReadLine()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			panic(err)
+		}
+		res = append(res, parseLine(string(line)))
+	}
+	return res
+}
+
+func parseLine(s string) []int {
+	var res []int
+	split := strings.Split(s, " ")
+	for i := 0; i < len(split); i++ {
+		a, err := strconv.Atoi(split[i])
+		if err != nil {
+			panic(err)
+		}
+		res = append(res, a)
+	}
+	return res
+}
+func FirstCol(both [][]int) (res []int) {
+	for i := 0; i < len(both); i++ {
+		res = append(res, both[i][0])
+	}
+	return
+}
+
+func SecondCol(both [][]int) (res []int) {
+	for i := 0; i < len(both); i++ {
+		res = append(res, both[i][1])
+	}
+	return
+}
+
+func Doit() int {
+	fi, err := os.Open("../d1/input1.txt")
+	if err != nil {
+		panic(err)
+	}
+	// close fi on exit and check for its returned error
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	input := ParseInput(fi)
+	return ComputeDistance(FirstCol(input), SecondCol(input))
 }
--- main/main.go
+++ main/main.go
@@ -12,6 +12,7 @@ func main() {
 	a := []int{3, 4, 2, 1, 3, 3}
 	b := []int{3, 4, 5, 3, 9, 3}
 	fmt.Println(d1.ComputeDistance(a, b))
+	fmt.Println(d1.Doit())
 
 	fi, err := os.Open("../d2/input.txt")
 	if err != nil {
